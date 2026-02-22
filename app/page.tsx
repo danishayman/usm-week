17dc0c0d42aea1bd9c5bd352b564069c47e61bb8
danishayman <danishaiman3b@gmail.com>
2026-02-23 01:06:21 +0800

    update USM title to be more visible

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
           className="object-contain"
           priority
         />
-        <p className="font-sans text-xs font-semibold uppercase tracking-widest text-slate-400">
+        <p className="font-sans text-xs font-semibold uppercase tracking-widest text-slate-800">
           Universiti Sains Malaysia
         </p>
       </div>
